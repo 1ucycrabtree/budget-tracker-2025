e37f98d0b5823b60470a619db61dc21f2224ea3b
lc2353 <120254125+lc2353@users.noreply.github.com>
2025-10-05 16:48:49 +0100

    fix postcss

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,9 +1,5 @@
-import tailwindcssPostcss from '@tailwindcss/postcss';
-import autoprefixer from 'autoprefixer';
-
 export default {
-  plugins: [
-    tailwindcssPostcss(),
-    autoprefixer,
-  ],
-};
+  plugins: {
+    "@tailwindcss/postcss": {},
+  }
+}
